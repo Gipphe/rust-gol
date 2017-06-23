a75e5df42d3ceeb6144e2efe2c482b1bb3a4a370
Victor Nascimento Bakke <gipphe@gmail.com>
2017-06-23 12:42:28 +0200

    Add count_neighbors and tests

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,16 @@ fn count_neighbors(board: &Vec<Vec<bool>>, cell_y: i32, cell_x: i32) -> i32 {
             Some(ref row) => {
 
                 for x in -1..2 {
-                    if x == y {
+                    if x == 0 && y == 0 {
                         continue;
                     }
                     let real_x: usize = (cell_x + x) as usize;
                     let neighbor: Option<&bool> = row.get(real_x);
 
                     match neighbor {
-                        None => (),
+                        None => {
+                            ()
+                        },
                         Some(neighbor) => {
                             if *neighbor {
                                 count += 1;
@@ -31,10 +33,68 @@ fn count_neighbors(board: &Vec<Vec<bool>>, cell_y: i32, cell_x: i32) -> i32 {
 }
 
 fn main() {
-    let mut board: Vec<Vec<bool>> = vec![vec![false; 20]; 20];
-    board[0][0] = true;
-    board[0][2] = true;
-    let count = count_neighbors(&board, 0, 1);
-    (-1..2).iter().fo
-    println!("{:?}", count);
+    let board: Vec<Vec<bool>> = vec![vec![true; 20]; 20];
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn make_board() -> Vec<Vec<bool>> {
+        vec![vec![false; 20]; 20]
+    }
+    fn make_alive_board() -> Vec<Vec<bool>> {
+        vec![vec![true; 20]; 20]
+    }
+    #[test]
+    fn counts_no_alive_cells_for_dead_board() {
+        let board: Vec<Vec<bool>> = make_board();
+        let mut result: i32 = count_neighbors(&board, 0, 0);
+        assert_eq!(0, result);
+        result = count_neighbors(&board, 10, 10);
+        assert_eq!(0, result);
+    }
+    #[test]
+    fn counts_correct_alive_neighbors_for_corner_cells() {
+        let mut board: Vec<Vec<bool>> = make_board();
+        board[0][1] = true;
+        board[1][0] = true;
+        let result: i32 = count_neighbors(&board, 0, 0);
+        assert_eq!(2, result);
+    }
+    #[test]
+    fn counts_correct_alive_neighbors_for_edge_cells() {
+        let mut board: Vec<Vec<bool>> = make_board();
+        board[0][1] = true;
+        board[1][2] = true;
+        board[1][3] = true;
+        let result: i32 = count_neighbors(&board, 0, 2);
+        assert_eq!(3, result);
+    }
+    #[test]
+    fn counts_correct_alive_neighbors_for_far_corner_cell() {
+        let mut board: Vec<Vec<bool>> = make_board();
+        board[19][18] = true;
+        board[18][19] = true;
+        let result: i32 = count_neighbors(&board, 19, 19);
+        assert_eq!(2, result);
+    }
+    #[test]
+    fn counts_max_number_of_neighbors_for_alive_board() {
+        let board: Vec<Vec<bool>> = make_alive_board();
+        let result: i32 = count_neighbors(&board, 10, 10);
+        assert_eq!(8, result);
+    }
+    #[test]
+    fn counts_correct_max_neighbors_for_corner_cell() {
+        let board: Vec<Vec<bool>> = make_alive_board();
+        let result: i32 = count_neighbors(&board, 0, 0);
+        assert_eq!(3, result);
+    }
+    #[test]
+    fn counts_correct_max_neighbors_for_far_corner_cell() {
+        let board: Vec<Vec<bool>> = make_alive_board();
+        let result: i32 = count_neighbors(&board, 19, 19);
+        assert_eq!(3, result);
+    }
 }
